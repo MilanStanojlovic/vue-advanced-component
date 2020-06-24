8b5a15f17f2f15c5f96392871b56c95d5bfe38e0
Milan <mistikone1@gmail.com>
2020-06-24 12:41:39 +0200

    advanced component usage

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="coll-xs-12">
+        <app-quote>
+          <h1 slot="title">{{ quoteTitle }}</h1>
+          <p slot="content">A wonderful quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote";
+export default {
+  components: {
+    "app-quote": Quote
+  },
+
+  data: () => {
+    return {
+      quoteTitle: "The Quote"
+    };
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
